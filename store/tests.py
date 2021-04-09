e4c0df75e53e6909d09c5233d490b40dd150a894
Vladyslav Kotsiuba <vladyslav.kotsiuba@gmail.com>
2021-04-10 00:29:31 +0300

    Tested Category and Product models

--- store/tests.py
+++ store/tests.py
@@ -1,3 +1,46 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from store.models import Category, Product
 
-# Create your tests here.
+
+class TestCategoriesModel(TestCase):
+    def setUp(self):
+        self.data1 = Category.objects.create(name='django', slug='django')
+
+    def test_category_model_entry(self):
+        """
+        Test Category model data insertion/types/field attributes
+        """
+        data = self.data1
+        self.assertTrue(isinstance(data, Category))
+
+    def test_category_model_string(self):
+        """
+        Test Category model default name
+        """
+        data = self.data1
+        self.assertEqual(str(data), 'django')
+
+
+class TestProductModel(TestCase):
+    def setUp(self):
+        Category.objects.create(name='django', slug='django')
+        User.objects.create(username='admin')
+        self.data1 = Product.objects.create(
+            category_id=1, title='django beginners', created_by_id=1,
+            slug='django-beginners', price='120.00', image='django'
+        )
+
+    def test_products_model_entry(self):
+        """
+        Test Product model data insertion/types/field attributes
+        """
+        data = self.data1
+        self.assertTrue(isinstance(data, Product))
+
+    def test_products_model_string(self):
+        """
+        Test Product model default name
+        """
+        data = self.data1
+        self.assertEqual(str(data), 'django beginners')
